a17126c395bef3ec44f7e8887ee2031d8d57dede
TatsianaKedrova <Baletrot@gmail.com>
2021-07-25 12:58:36 +0200

    Updates

---
+++ precache-manifest.3f66fd6e9b8003e463950ed8c14ed2ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a805c2217ad8ceaeaa2ec2a8467fc7a5",
+    "url": "/todolist-it-incubator/index.html"
+  },
+  {
+    "revision": "03c2bc4396671d4360ad",
+    "url": "/todolist-it-incubator/static/css/main.8a08bab7.chunk.css"
+  },
+  {
+    "revision": "abc0e18875ddd0076a0d",
+    "url": "/todolist-it-incubator/static/js/2.c4e4fb49.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/todolist-it-incubator/static/js/2.c4e4fb49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03c2bc4396671d4360ad",
+    "url": "/todolist-it-incubator/static/js/main.1f291c24.chunk.js"
+  },
+  {
+    "revision": "c58f22cddfcc855eae1d",
+    "url": "/todolist-it-incubator/static/js/runtime-main.1486da19.js"
+  }
+]);
